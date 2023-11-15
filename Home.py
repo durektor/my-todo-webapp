8251799e6baa5a1a0a06018f624f89c8a99bf1d7
jim <jim.beale@outlook.com>
2023-11-15 11:40:16 -0500

    Add configuration files

--- Home.py
+++ Home.py
@@ -2,6 +2,7 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+st.set_page_config(layout="wide")
 
 
 def add_todo():
@@ -13,7 +14,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="Enter a todo", placeholder="Add new todo...",
+              label_visibility="hidden", on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,7 +27,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-
-st.text_input(label="Enter a todo", placeholder="Add new todo...",
-              label_visibility="hidden", on_change=add_todo, key="new_todo")
-
